4b241777bbfcc8cefac280c8bc746ab6efc6e312
Zlotnik-Ilya <ilya.zlotnik@mail.ru>
2023-02-13 17:05:42 +0300

    feat(get-packs): test

--- src/features/Packs/Packs.tsx
+++ src/features/Packs/Packs.tsx
@@ -1,38 +1,19 @@
 import React, {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from '../../app/store';
-//import {setPacksTC} from './packs-redicer';
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material';
-import {fetchPacks} from './packs-redicer';
+import {setPacksTC} from './packs-redicer';
 
 export const Packs = () => {
 
   const dispatch = useAppDispatch()
-  const packs = useAppSelector(state => state.packs.packs)
+  const packs = useAppSelector(state => state.packs.packs.cardPacks)
 
   console.log('packs', packs)
-
-  // function createData(
-  //   name: string,
-  //   calories: number,
-  //   fat: number,
-  //   carbs: number,
-  //   protein: number,
-  // ) {
-  //   return { name, calories, fat, carbs, protein };
-  // }
-
-  // const rows = [
-  //   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  //   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  //   createData('Eclair', 262, 16.0, 24, 6.0),
-  //   createData('Cupcake', 305, 3.7, 67, 4.3),
-  //   createData('Gingerbread', 356, 16.0, 49, 3.9),
-  // ];
 
 
 
   useEffect(() => {
-   dispatch(fetchPacks({}))
+   dispatch(setPacksTC())
   },[])
 
 
@@ -51,9 +32,8 @@ export const Packs = () => {
         <TableBody>
           {packs.map((row) => (
             <TableRow
-              key={row.name}
+              key={row._id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-
               <TableCell
                 component="th" scope="row">{row.name}</TableCell>
               <TableCell
@@ -62,11 +42,8 @@ export const Packs = () => {
               <TableCell align="right">{row.user_name}</TableCell>
               {/*<TableCell align="right">{row.}</TableCell>*/}
             </TableRow>
-
           ))}
         </TableBody>
-
-
 
       </Table>
     </TableContainer>
--- src/features/Packs/packs-redicer.ts
+++ src/features/Packs/packs-redicer.ts
@@ -3,59 +3,84 @@ import {errorUtil} from '../../common/utils/error utils';
 import {AxiosError} from 'axios';
 import {setIsLoggedInAC} from '../Login/auth-reducer';
 import {packsAPI, PackType} from '../../api/paksAPI';
+import {AppRootStateType} from '../../app/store';
 
 
-type InitState = {
-  packs: PackType[]
-}
+// type InitState = {
+//   packs: PackType[],
+//   queryParams: {
+//     pageCount:number
+//     sort:string
+//     page:number
+//     packName:string
+//     isMyPacks: boolean
+//     min:number
+//     max:number
+//     userId: null
+//   }
+// }
 
 
-const initialState: InitState = {
-  packs: []
-}
-
-
-export const fetchPacks = createAsyncThunk(
-  'packs/fetchByIdStatus',
-  async (params: {}, {dispatch, getState}) => {
-    try {
-      const {data} = await packsAPI.getPacks(params)
-      return  data
-    } catch (e: any) {
-
-    }
-
-
+const initialState = {
+  packs: {
+    cardPacks: []as PackType[]
+  },
+    queryParams: {
+    pageCount: 0,
+    sortPacks: '',
+    min: 0,
+    max: 4,
+    page: 0,
+    packName: '',
+    user_id: ''
   }
-)
+}
+
+
+// export const fetchPacks = createAsyncThunk(
+//   'packs/fetchByIdStatus',
+//   async (params: {}, {dispatch, getState}) => {
+//     try {
+//       const {data} = await packsAPI.getPacks(params)
+//       return  data
+//     } catch (e: any) {
+//
+//     }
+//   }
+// )
 
 const slice = createSlice({
   name: 'packs',
   initialState: initialState,
   reducers: {
-    // setPacksAC(state, action) {
-    //   state.packs = action.payload
-    // }
+    setPacksAC(state, action) {
+      state.packs = action.payload
+    }
   },
-   extraReducers: builder => {
-    builder.addCase(fetchPacks.fulfilled, (state, action) => {
-      if(action.payload) state.packs = action.payload.cardPacks
-    })
-   }
+  // extraReducers: builder => {
+  //  builder.addCase(fetchPacks.fulfilled, (state, action) => {
+  //    if(action.payload) state.packs = action.payload.cardPacks
+  //  })
+  // }
 })
 
 export const setPacksReducer = slice.reducer
-//export const {setPacksAC} = slice.actions
+export const {setPacksAC} = slice.actions
 
 
 //thunks
-// export const setPacksTC = (data: any) => async (dispatch: Dispatch) => {
-//   try {
-//     const res = await packsAPI.getPacks(data)
-//     // console.log(res.data.cardPacks)
-//     dispatch(setIsLoggedInAC({isLoggedIn: true}))
-//     dispatch(setPacksAC({packs: res.data}))
-//   } catch (e) {
-//     errorUtil(e as Error | AxiosError<{ error: string }>, dispatch)
-//   }
-// }
+export const setPacksTC = () => async (dispatch: Dispatch, getState: () => AppRootStateType) => {
+
+  const {
+     packName, sortPacks, user_id,min,max,pageCount,page
+  } = getState().packs.queryParams
+
+  try {
+    const res = await packsAPI.getPacks({ packName, sortPacks, user_id,min,max,pageCount,page})
+    // console.log(res.data.cardPacks)
+    dispatch(setIsLoggedInAC({isLoggedIn: true}))
+    dispatch(setPacksAC({packs: res.data}))
+  } catch (e) {
+    errorUtil(e as Error | AxiosError<{ error: string }>, dispatch)
+  }
+}
