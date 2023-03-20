575d375590916a3df91bcefbf013b219229d08c9
Stemmy287 <steem287@gmail.com>
2023-03-20 18:20:10 +0300

    fix: padding bottom

--- src/app/pages/Pages.tsx
+++ src/app/pages/Pages.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import s from './Pages.module.scss'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { PATH } from '../../common/constants/path'
-import { Login } from '../../features/Auth/Login/Login'
-import { Registration } from '../../features/Auth/Registration/Registration'
-import { Profile } from '../../features/Profile/Profile'
-import { PasswordRecovery } from '../../features/Auth/PasswordRecovery/PasswordRecovery'
-import { NewPassword } from '../../features/Auth/NewPassword/NewPassword'
-import { Error404 } from '../../common/components/Error404/Error404'
-import { CheckEmail } from "../../features/Auth/CheckEmail/CheckEmail"
-import { Packs } from '../../features/Packs/Packs'
-import { Cards } from "../../features/Packs/Cards/Cards"
+import { PATH } from 'common/constants/path'
+import { Login } from 'features/Auth/Login/Login'
+import { Registration } from 'features/Auth/Registration/Registration'
+import { Profile } from 'features/Profile/Profile'
+import { PasswordRecovery } from 'features/Auth/PasswordRecovery/PasswordRecovery'
+import { NewPassword } from 'features/Auth/NewPassword/NewPassword'
+import { Error404 } from 'common/components/Error404/Error404'
+import { CheckEmail } from "features/Auth/CheckEmail/CheckEmail"
+import { Packs } from 'features/Packs/Packs'
+import { Cards } from "features/Packs/Cards/Cards"
 import { LearnPage } from 'features/Packs/LearnPage/LearnPage'
 
 export const Pages = () => {
